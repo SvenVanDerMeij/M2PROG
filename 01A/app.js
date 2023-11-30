e7f5f4eaf48b93f1e9365e2dea4f79eee78666da
Sven van der Meij <sven.van.der.meij@hva.nl>
2023-11-30 13:02:39 +0100

    les2

--- 01A/app.js
+++ 01A/app.js
@@ -2,7 +2,20 @@ class App
 {
     runApplication()
     {
-        console.log("hello world!");
+        let appNaam = "bub"
+        let version = "0.9"
+        let versionDate = new Date("2023-11-30")
+        let author = "Svennie van der Meij"
+        let copyright = "SvennieIncorporated"
+        let distributeur = "Disney"
+        let darkMode = false
+        console.log(appNaam);
+        console.log(version)
+        console.log(versionDate)
+        console.log(author)
+        console.log(copyright)
+        console.log(distributeur)
+        console.log(darkMode)
     }
 }
 
