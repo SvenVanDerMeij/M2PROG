113224653ede0dbf94d0cabf291bfdff007b611b
Sven van der Meij <sven.van.der.meij@hva.nl>
2023-12-08 12:15:29 +0100

    lesdeel1

---
+++ 02 canvashuis/app.js
@@ -0,0 +1,13 @@
+class App
+{
+    runApplication()
+    {
+        let canvas = document.getElementById("canvasId")
+        let g = canvas.getContext("2d");
+        g.fillRect(0,0,10,10)
+     console.log(canvas)
+    }
+}
+
+let app = new App();
+app.runApplication();
