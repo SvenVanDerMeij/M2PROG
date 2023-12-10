d0ab903b41900c27e0e285c869e39dcf7a823722
Sven van der Meij <sven.van.der.meij@hva.nl>
2023-12-10 18:33:50 +0100

    les4deel1

---
+++ 04functions/app.js
@@ -0,0 +1,19 @@
+function letterlijkDeBesteCodeOoitGeschreven()
+{
+    console.log("Hey I just met you, and this is crazy")
+    console.log("call me maybe")
+}
+        class App
+{
+    runApplication()
+    {
+     console.log("hello world")
+    }
+}
+
+let app = new App();
+app.runApplication();
+
+letterlijkDeBesteCodeOoitGeschreven();
+letterlijkDeBesteCodeOoitGeschreven();
+letterlijkDeBesteCodeOoitGeschreven();
