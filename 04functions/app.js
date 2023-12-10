5b5696e4e02f9531529f5dc367ef1a3618a4b023
Sven van der Meij <sven.van.der.meij@hva.nl>
2023-12-10 20:35:23 +0100

    les4deel3

--- 04functions/app.js
+++ 04functions/app.js
@@ -1,3 +1,17 @@
+function Bestaat()
+{
+    let uitkomst =1;
+    return uitkomst
+}
+function BerekenMaar1UitkomstVanXOmdatJavascriptNiet2WaardesMagReturnenOokalZijnErTweeAntwoordenSorry(a, b, c)
+{
+    let D = (b*b) - 4*a*c
+    let x = (-b-Math.sqrt(D))/(2*a)
+    let x2 = ((-b+Math.sqrt(D))/(2*a))
+    return x && x2
+    
+}
+
 function slechteCode(zin)
 {
     console.log("deze zin is technisch gezien uniek")
@@ -26,3 +40,12 @@ letterlijkDeBesteCodeOoitGeschreven();
 slechteCode("sick");
 slechteCode("leip")
 slechteCode("radical")
+let shakespear = Bestaat()
+console.log(shakespear)
+console.log(Bestaat());
+let antwoord1 = BerekenMaar1UitkomstVanXOmdatJavascriptNiet2WaardesMagReturnenOokalZijnErTweeAntwoordenSorry(5, 18, 9)
+console.log(antwoord1)
+let antwoord2 = BerekenMaar1UitkomstVanXOmdatJavascriptNiet2WaardesMagReturnenOokalZijnErTweeAntwoordenSorry(7, 9, 2)
+console.log(antwoord2)
+let antwoord3 = BerekenMaar1UitkomstVanXOmdatJavascriptNiet2WaardesMagReturnenOokalZijnErTweeAntwoordenSorry(1, 1500, 0)
+console.log(antwoord3)
