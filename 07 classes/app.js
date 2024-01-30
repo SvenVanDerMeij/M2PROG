e99e81dd8986021000ef75497fd4fbd044d5edaf
Sven van der Meij <sven.van.der.meij@hva.nl>
2024-01-30 23:24:22 +0100

    les7begin

---
+++ 07 classes/app.js
@@ -0,0 +1,25 @@
+
+class App {
+    runApplication() {
+
+    }
+}
+class Greet {
+    constructor() {
+        console.log("wat?patat")
+    }
+}
+class Goodbye {
+    constructor() {
+        console.log("pardon?friet")
+    }
+}
+
+let bye = new Goodbye()
+let greet = new Greet()
+new Greet()
+new Greet()
+new Greet()
+
+let app = new App();
+app.runApplication();
