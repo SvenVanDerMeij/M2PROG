8c20dbbe33db8d03161efed459e22f7af643e7d5
Sven van der Meij <sven.van.der.meij@hva.nl>
2024-01-30 23:02:16 +0100

    les 6 af

--- 06Events/app.js
+++ 06Events/app.js
@@ -5,13 +5,47 @@
     {
         
         const uiButton = document.getElementById("myButton")
-        let localeFunction = function (e)
-        {
-            console.log("click!")
-        };
-        uiButton.addEventListener("click",localeFunction);
+        uiButton.addEventListener("click", (e)=>
+        { 
+            const para = document.createElement("p");
+            const node = document.createTextNode("This is new.");
+            para.appendChild(node);
+            document.body.appendChild(para);
+        });
+        const blons = document.getElementById("monke")
+        blons.addEventListener("click", (e)=>
+        { 
+            const para = document.createElement("p");
+            const node = document.createTextNode("aap.");
+            const main = document.createElement("h1");
+            const nodey = document.createTextNode("AAP.");
+            const plaatje = document.createElement("img");
+            plaatje.src = 'SuperLeip.jpg';
+            document.body.appendChild(plaatje)
+            para.appendChild(node);
+            document.body.appendChild(para);
+            main.appendChild(nodey)
+            document.body.appendChild(main)
+
+        });
+        const uniek = document.getElementById("anders")
+        uniek.addEventListener("click", (e)=>
+        { 
+            const para = document.createElement("a");
+            const node = document.createTextNode("geinig filmpie");
+            const main = document.createElement("h1");
+            const nodey = document.createTextNode("WAJOOOOOOOOOO");
+            para.appendChild(node);
+            para.title = "my title text";
+            para.href = "https://www.youtube.com/watch?v=RHdGvWXfPCM";
+            document.body.appendChild(para);
+            main.appendChild(nodey);
+            document.body.appendChild(main)
+        });
+        }
+        
     }
-}
+
 
 let app = new App();
 app.runApplication();
