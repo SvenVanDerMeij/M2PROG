7cf441ab7bf7549f19bf285c88297d0a1494b8f1
Sven van der Meij <sven.van.der.meij@hva.nl>
2024-01-31 02:12:57 +0100

    0706

--- 07 dinos/app.js
+++ 07 dinos/app.js
@@ -1,34 +1,45 @@
-class Dino
-{
-    constructor(naam, leeftijd, vleeseter)
-    {
+class Dino {
+    constructor(naam, leeftijd, vleeseter) {
         this.naam = naam;
         this.leeftijd = leeftijd
         this.vleeseter = vleeseter
     }
-    eatFood(foodToEat)
-    {
-        foodToEat.leeft=false;
-        this.honger=false
+    eatFood(foodToEat) {
+        foodToEat.leeft = false;
+        this.honger = false;
         console.log("ik ben een " + this.naam)
+            console.log("ik eet nu " + foodToEat.naam)
+            
         console.log("mijn honger: " + this.honger)
     }
 }
- class App
-{
-    runApplication()
-    {
-       let dino = new Dino("Trex",20,true);
-       let plantenEter = new Dino("Triceratops",20,false)
-       console.log(dino)
-       console.log(plantenEter);
 
-       dino.eatFood(plantenEter);
-       console.log(dino.naam + " is " + dino.leeftijd + " jaar oud.")
-       console.log("leeft " + plantenEter.naam + "? "+ plantenEter.leeft)
-       console.log("en is "+ dino.naam+" een vleeseter?" + dino.vleeseter)
-       
-       
+class Plant {
+    constructor(naam,leeft){
+        this.naam = naam
+        this.levend = leeft
+    }
+}
+class App {
+    runApplication() {
+        let dino = new Dino("Trex", 20, true);
+        let plantenEter = new Dino("Triceratops", 20, false)
+        let gras = new Plant("gras", true)
+        let struik = new Plant("struik", true)
+        console.log(dino)
+        console.log(plantenEter);
+        console.log(gras)
+        console.log(struik)
+
+        
+        plantenEter.eatFood(gras)
+        plantenEter.eatFood(struik)
+        dino.eatFood(plantenEter);
+        console.log(dino.naam + " is " + dino.leeftijd + " jaar oud.")
+        console.log("leeft " + plantenEter.naam + "? " + plantenEter.leeft)
+        console.log("en is " + dino.naam + " een vleeseter?" + dino.vleeseter)
+
+
     }
 }
 
